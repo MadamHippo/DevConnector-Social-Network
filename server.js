ec0be7c5bc622feace6d6390a9bb2fa00c8cb249
MadamHippo <88348716+MadamHippo@users.noreply.github.com>
2021-10-18 01:03:41 -0700

    Connected to database + set up routes

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require('express'); //importing library to use
 const app = express(); // creating an instance of Express and naming it app
+const db = require('./config/keys');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
+
+//multi-threading is beautiful, let's write our first route
 //you have to say what request (got or post)
 //user came to the homepage and express will send a hello back
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.send('Hello World!'));
 
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+// .use meas USE these javascript files. It's saying, hey Express (app) use these routes.
+
+// Connect to db
+mongoose.connect(db.mongoURI)
+        .then(() => console.log('Mongodb connected'))
+        .catch((err) => console.log(err));
+
+// promise statement can be attached to any funciton call. .then() and .catch(). It's callled promoise because afer this function is executed, ONE of the then and catch WILL run.
+// You don't need both then and catch. Sometimes you don't care if something fails and nothing is there to catch it.
+
+
+// now by this point, there are 2 threads running. That's why Server is running on port printed BEFORE Mongodb printed as connected.
 const port = 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`) );
 //you're asking Express to listen at the port for a callback
